457a13426370e17e39208d98acbb2a9af62773df
alebus <spectralcreations@gmail.com>
2021-06-28 19:33:16 -0700

    init

---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite coin spawning function, asynchronous
+		StartCoroutine(SpawnGems());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnGems() {
+		while (true) {
+
+			// number of coins we could spawn vertically
+			int coinsThisRow = Random.Range(1, 3);
+
+			// instantiate all coins in this row separated by some random amount of space
+			for (int i = 0; i < coinsThisRow; i++) {
+				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			}
+
+			// 5 to 8 seconds before you get a gem
+			yield return new WaitForSeconds(Random.Range(5, 8));
+		}
+	}
+}
